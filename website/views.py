1af1ee85f97a47c28674f2b31af6c9203a1aacb7
Misha <mishkamba1@gmail.com>
2020-05-05 15:55:02 +0900

    forms, templates and static files adding

--- website/views.py
+++ website/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .forms import registrationForms
+from .forms import signinForms
 
 # Create your views here.
 def index(request):
-	return HttpResponse("Hello")
+	return render(request, "index.html")
+def registration(request):
+	registForm = registrationForms()
+	return render(request, "registation.html", {"form": registForm})
+def signin(request):
+	signinForm = signinForms()
+	return render(request, "signin.html", {"form": signinForms})
--- Сensus/urls.py
+++ Сensus/urls.py
@@ -19,5 +19,7 @@ from website import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('regist', views.registration),
+    path('signin', views.signin)
 ]
